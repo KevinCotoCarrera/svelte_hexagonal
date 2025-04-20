fe5766e36f12c4d465a7ab96ad0a9647c86fcaeb
Kevin Coto <kevincoto2002@gmail.com>
2025-04-20 14:24:07 +0700

    merge: conflicts solved

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import { sequence } from '@sveltejs/kit/hooks';
-import * as auth from '$lib/server/auth.js';
-import type { Handle } from '@sveltejs/kit';
-import { paraglideMiddleware } from '$lib/paraglide/server';
-=======
 // src/hooks.server.ts
 import { sequence } from '@sveltejs/kit/hooks';
 import type { Handle } from '@sveltejs/kit';
@@ -11,26 +5,17 @@ import { paraglideMiddleware } from '$lib/paraglide/server';
 import { SessionManager, sessionCookieName } from '$lib/infrastructure/auth/SessionManager';
 
 const sessionManager = new SessionManager();
->>>>>>> bundles/master
 
 const handleParaglide: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request, locale }) => {
 		event.request = request;
-<<<<<<< HEAD
-
-=======
->>>>>>> bundles/master
 		return resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
 		});
 	});
 
 const handleAuth: Handle = async ({ event, resolve }) => {
-<<<<<<< HEAD
-	const sessionToken = event.cookies.get(auth.sessionCookieName);
-=======
 	const sessionToken = event.cookies.get(sessionCookieName);
->>>>>>> bundles/master
 
 	if (!sessionToken) {
 		event.locals.user = null;
@@ -38,21 +23,12 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 		return resolve(event);
 	}
 
-<<<<<<< HEAD
-	const { session, user } = await auth.validateSessionToken(sessionToken);
-
-	if (session) {
-		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
-	} else {
-		auth.deleteSessionTokenCookie(event);
-=======
 	const { session, user } = await sessionManager.validate(sessionToken);
 
 	if (session) {
 		sessionManager.setCookie(event, sessionToken, session.expiresAt);
 	} else {
 		sessionManager.deleteCookie(event);
->>>>>>> bundles/master
 	}
 
 	event.locals.user = user;
